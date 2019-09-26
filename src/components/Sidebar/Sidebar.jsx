1196fb78ffc89cacfac76a9e97c8aba40d939794
Kernael92 <kernaelapuko5@gmail.com>
2019-09-26 23:51:38 +0300

    update the sidebar.jsx file

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,254 +1,153 @@
 import React from "react";
-import { NavLink as NavLinkRRD, Link } from "react-router-dom";
-// nodejs Library to set properties for components
+import { NavLink, Link } from "react-router-dom";
+// nodejs library to set properties for components
 import { PropTypes } from "prop-types";
 
-// reactstrap components
-import { 
-    Button,
-    Card,
-    CardHeader,
-    CardBody,
-    CardTitle,
-    Collapse,
-    DropdownMenu,
-    DropdownItem,
-    UncontrolledDropdown,
-    DropdownToggle,
-    FormGroup,
-    Form,
-    Input,
-    InputGroupAddon,
-    InputGroupText,
-    InputGroup,
-    Media,
-    NavbarBrand,
-    Navbar,
-    NavItem,
-    NavLink,
-    Nav,
-    Progress,
-    Table,
-    Container,
-    Row,
-    Col
-} from "reactstrap";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
 
-let ps;
+// reactstrap components
+import { Nav } from "reactstrap";
+
+var ps;
 
 class Sidebar extends React.Component {
-
-    state = {
-        collapseOpen: false
-    };
-    constructor(props) {
-        super(props);
-            this.activeRoute.bind(this);
+  constructor(props) {
+    super(props);
+    this.activeRoute.bind(this);
+  }
+  // verifies if routeName is the one active (in browser input)
+  activeRoute(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
+  componentDidMount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(this.refs.sidebar, {
+        suppressScrollX: true,
+        suppressScrollY: false
+      });
     }
-    //  Verifies if routeName is the one active (in browser input)
-    activeRoute(routeName) {
-        return this.props.location.pathname.indexof(routeName) > -1 ? "active" : "";
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps.destroy();
     }
-    // toggles collapse between opened and closed (true/false)
-    toggleCollapse = () => {
-        this.setState({
-            collapseOpen: !this.state.collapseOpen
-        });
-    };
-    // closes the collapse
-    closeCollapse = () => {
-        this.setState({
-            collapseOpen: false
-        });
-    };
-    // creates the links that appear in the left menu / Sidebar
-    createLinks = routes => {
-        return routes.map((prop, key) => {
-            return (
-                <NavItem key={key}>
-                    <NavLink 
-                        to={prop.layout + prop.path}
-                        tag={NavLinkRRD}
-                        onClick={this.closeCollapse}
-                        activeClassName="active"
-                    >
-                    <i className={prop.icon} />>
-                        {prop.name}
-                    </NavLink>
-                </NavItem>
-            );
-        });
-    };
-    render() {
-        const { bgColor, routes, logo } = this.props;
-        let navbarBrandProps;
-        if (logo && logo.innerLink) {
-            navbarBrandProps = {
-                to: logo.innerLink,
-                tag: Link
-            };
-        } else if (logo && logo.outterLink) {
-            navbarBrandProps = {
-                href: logo.outerLink,
-                target: "_blank"
-            };
-        }
-        return (
-            <Navbar
-                className="navbar-vertical fixed-left navbar-light bg-white"
-                expand="md"
-                id="sidenav-main"
-            >
-                <Container fluid>
-                    {/* {Toggler} */}
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        onClick={this.toggleCollapse}
-                    >
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    {/* {Brand} */}
-                    {logo ? (
-                        <NavbarBrand className="pt-0" {...navbarBrandProps}>
-                            <img 
-                                alt={logo.imgAlt}
-                                className="navbar-brand-img"
-                                src={logo.imgSrc}
-                            />
-                        </NavbarBrand>
-                    ) : null}
-                    {/* {User} */}
-                    <Nav className="align-items-center d-md-none">
-                        <UncontrolledDropdown nav>
-                            <DropdownToggle nav className="nav-link-icon">
-                                <i classNmae="ni ni-bell-55" />
-                            </DropdownToggle>
-                            <DropdownMenu
-                                aria-labelledby="navbar-default_dropdown_1"
-                                className="dropdowN-menu-arrow"
-                                right
-                            >
-                                <DropdownItem>Action</DropdownItem>
-                                <DropdownItem>Another action</DropdownItem>
-                                <DropdownItem divider/>
-                                <DropdownItem>Somethinf else here</DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                        <UncontrolledDropdown nav>
-                            <DropdownToggle nav>
-                                <Media className="align-items-center">
-                                    <span className="avatar avatar-sm rounded-circle">
-                                        <img 
-                                            alt="..."
-                                            src={require("")}
-                                        />
-                                    </span>
-                                </Media>
-                            </DropdownToggle>
-                            <DropdownMenu className="dropdown-menu-arrow" right>
-                                <DropdownItem className="noti-title" header tag="div">
-                                    <h6 className="text-overflow m-0">Welcome!</h6>
-                                </DropdownItem>
-                                <DropdownItem to="/admin/page/profile" tag={Link}>
-                                    <i className="ni ni-single-02" />
-                                    <span>My Profile</span>
-                                </DropdownItem>
-                                <DropdownItem to="/admin/page/settings" tag={Link}>
-                                    <i className="ni ni-settings-gear-65" />
-                                    <span>Settings</span>
-                                </DropdownItem>
-                                <DropdownItem to="/admin/page/activity" tag={Link}>
-                                    <i className="ni ni-calendar-grid-58"/>
-                                    <span>Activity</span>
-                                </DropdownItem>
-                                <DropdownItem to="/admin/page/support" tag={Link}>
-                                    <i className="ni ni-support-16"/>
-                                    <span>Support</span>
-                                </DropdownItem>
-                                <DropdownItem divider/>
-                                <DropdownItem href="#" onClick={e => e.preventDefault()}>
-                                    <i className="ni ni-user-run"/>
-                                    <span>Logout</span>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    {/* {Collapse} */}
-                    <Collapse navbar isOpen={this.state.collapseOpen}>
-                        {/* {Collapse header} */}
-                        <div className="navbar-collapse-header d-md-none">
-                            <Row>
-                                {logo ? (
-                                    <Col className="collapse-brand" xs="6">
-                                    {logo.innerLink ? (
-                                        <Link to={logo.innerLink}>
-                                            <img src={logo.imgSrc} alt={logo.imgAlt} />
-                                        </Link>
-                                    ) : (
-                                        <a href={logo.outterLink}>
-                                            <img src={logo.imgSrc} alt={logo.imgAlt} />
-                                        </a>
-                                    )}
-                                    </Col>
-                                ) : null }
-                                <Col className="collapse-close" xs="6">
-                                    <button
-                                        className="navbar-toggler"
-                                        type="button"
-                                        onClick={this.toggleCollapse}
-                                    >
-                                    <span />
-                                    <span />
-                                    </button>
-                                </Col>
-                            </Row>
-                        </div>
-                        {/* {Form} */}
-                        <Form className="mt-4 mb-3 d-md-none">
-                            <InputGroup className="input-group-rounded input-group-merge">
-                                <Input
-                                    aria-label="Search"
-                                    className="form-control-rounded form-control-prepened"
-                                    placeholder="Search"
-                                    type="search"
-                                />
-                                <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                        <span className="fa fa-search" />
-                                    </InputGroupText>
-                                </InputGroupAddon>
-                            </InputGroup>
-                        </Form>
-                    </Collapse>
-                </Container>
-
-            </Navbar>
+  }
+  linkOnClick = () => {
+    document.documentElement.classList.remove("nav-open");
+  };
+  render() {
+    const { bgColor, routes, rtlActive, logo } = this.props;
+    let logoImg = null;
+    let logoText = null;
+    if (logo !== undefined) {
+      if (logo.outterLink !== undefined) {
+        logoImg = (
+          <a
+            href={logo.outterLink}
+            className="simple-text logo-mini"
+            target="_blank"
+            onClick={this.props.toggleSidebar}
+          >
+            <div className="logo-img">
+              <img src={logo.imgSrc} alt="react-logo" />
+            </div>
+          </a>
         );
+        logoText = (
+          <a
+            href={logo.outterLink}
+            className="simple-text logo-normal"
+            target="_blank"
+            onClick={this.props.toggleSidebar}
+          >
+            {logo.text}
+          </a>
+        );
+      } else {
+        logoImg = (
+          <Link
+            to={logo.innerLink}
+            className="simple-text logo-mini"
+            onClick={this.props.toggleSidebar}
+          >
+            <div className="logo-img">
+              <img src={logo.imgSrc} alt="react-logo" />
+            </div>
+          </Link>
+        );
+        logoText = (
+          <Link
+            to={logo.innerLink}
+            className="simple-text logo-normal"
+            onClick={this.props.toggleSidebar}
+          >
+            {logo.text}
+          </Link>
+        );
+      }
     }
+    return (
+      <div className="sidebar" data={bgColor}>
+        <div className="sidebar-wrapper" ref="sidebar">
+          {logoImg !== null || logoText !== null ? (
+            <div className="logo">
+              {logoImg}
+              {logoText}
+            </div>
+          ) : null}
+          <Nav>
+            {routes.map((prop, key) => {
+              if (prop.redirect) return null;
+              return (
+                <li
+                  className={
+                    this.activeRoute(prop.path) +
+                    (prop.pro ? " active-pro" : "")
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.layout + prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                    onClick={this.props.toggleSidebar}
+                  >
+                    <i className={prop.icon} />
+                  </NavLink>
+                </li>
+              );
+            })}
+          </Nav>
+        </div>
+      </div>
+    );
+  }
 }
 
 Sidebar.defaultProps = {
-    routes: [{}]
+  bgColor: "primary",
+  routes: [{}]
 };
 
 Sidebar.propTypes = {
-    // links that will be displayed inside the component
-    routes: PropTypes.arrayOf(PropTypes.object),
-    logo: PropTypes.shape({
-      // innerLink is for links that will direct the user within the app
-      // it will be rendered as <Link to="...">...</Link> tag
-      innerLink: PropTypes.string,
-      // outterLink is for links that will direct the user outside the app
-      // it will be rendered as simple <a href="...">...</a> tag
-      outterLink: PropTypes.string,
-      // the image src of the logo
-      imgSrc: PropTypes.string.isRequired,
-      // the alt for the img
-      imgAlt: PropTypes.string.isRequired
-    })
-  };
-  
-  export default Sidebar;
+  // if true, then instead of the routes[i].name, routes[i].rtlName will be rendered
+  // insde the links of this component
+  rtlActive: PropTypes.bool,
+  bgColor: PropTypes.oneOf(["primary", "blue", "green"]),
+  routes: PropTypes.arrayOf(PropTypes.object),
+  logo: PropTypes.shape({
+    // innerLink is for links that will direct the user within the app
+    // it will be rendered as <Link to="...">...</Link> tag
+    innerLink: PropTypes.string,
+    // outterLink is for links that will direct the user outside the app
+    // it will be rendered as simple <a href="...">...</a> tag
+    outterLink: PropTypes.string,
+    // the text of the logo
+    text: PropTypes.node,
+    // the image src of the logo
+    imgSrc: PropTypes.string
+  })
+};
 
-
+export default Sidebar;
