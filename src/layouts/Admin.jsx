ff93efd4d65c9df44e3cd1bfcebe24c01e8f8028
Kernael92 <kernaelapuko5@gmail.com>
2019-09-26 11:14:27 +0300

    setting properties for the components in the sidebar.jsx file

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -3,3 +3,6 @@ import { React, Switch } from "react-router-dom";
 // reactstrap components
 import { Container } from "reactstrap";
 // core components
+import AdminNavbar from "components/Navbars/AdminNavbar.jsx";
+
+
