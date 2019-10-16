4fe7d8220988b88f70b28abc3293ac523ef7d853
Apuko <kernael.apuko@certeon.co.ke>
2019-10-16 14:23:15 +0300

    add folders to the app folder for easier collaboration

---
+++ app/common/constants.py
@@ -0,0 +1,10 @@
+COLLECTIONS = {
+    'PHISHING': 'phishing',
+    'SOCIAL':'social',
+    'MALWARE':'malware',
+    'PHYSICAL': 'physical',
+    'MOBILE': 'mobile',
+    'BROWSING':'browsing',
+    'SAFETY': 'safety',
+    'REMOTELY': 'remotely'
+}
