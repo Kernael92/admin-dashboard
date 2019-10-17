660fb1a2606ef4847d25c4861dcbb21b27ca907f
Apuko <kernael.apuko@certeon.co.ke>
2019-10-17 16:13:57 +0300

    create configurations for development and production

--- src/views/Phishing.jsx
+++ src/views/Phishing.jsx
@@ -100,60 +100,6 @@ class Phishing extends React.Component {
                             </CardFooter>
                         </Card>
                         </Col>
-                        <Col lg="6">
-                        <Card className="card">
-                            <CardHeader>
-                            <h5 className="card-category">Phishing101</h5>
-                            <CardTitle tag="h3">
-                                <p>Phishing101 Materials</p>
-                            </CardTitle>
-                            </CardHeader>
-                            <CardBody>
-                            <div className="material-details">
-                                <p>Phishing101</p>   
-                            </div>
-                            </CardBody>
-                            <CardFooter>
-                                <a href="/admin/Phishing101" class="btn btn-primary">Phishing101</a>
-                            </CardFooter>
-                        </Card>
-                        </Col>
-                        <Col lg="6">
-                        <Card className="card">
-                            <CardHeader>
-                            <h5 className="card-category">Phishing101</h5>
-                            <CardTitle tag="h3">
-                                <p>Phishing101 Materials</p>
-                            </CardTitle>
-                            </CardHeader>
-                            <CardBody>
-                            <div className="material-details">
-                                <p>Phishing101</p>   
-                            </div>
-                            </CardBody>
-                            <CardFooter>
-                                <a href="/admin/Phishing101" class="btn btn-primary">Phishing101</a>
-                            </CardFooter>
-                        </Card>
-                        </Col>
-                        <Col lg="6">
-                        <Card className="card">
-                            <CardHeader>
-                            <h5 className="card-category">Phishing101</h5>
-                            <CardTitle tag="h3">
-                                <p>Phishing101 Materials</p>
-                            </CardTitle>
-                            </CardHeader>
-                            <CardBody>
-                            <div className="material-details">
-                                <p>Phishing101</p>   
-                            </div>
-                            </CardBody>
-                            <CardFooter>
-                                <a href="/admin/Phishing101" class="btn btn-primary">Phishing101</a>
-                            </CardFooter>
-                        </Card>
-                        </Col>
                     </Row>                
                 </div>
             </>
